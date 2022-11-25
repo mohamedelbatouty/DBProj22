6cfac8e2f3ac5e6166bd3295b5e1611b436ebace
mohamedelbatouty <55361505+mohamedelbatouty@users.noreply.github.com>
2022-11-25 17:43:17 -0500

    Add files via upload

---
+++ Report.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Report {
+	public static void StudentReport(Connection conn, String Nnumber) {
+		// TODO Auto-generated method stub
+		
+	}
+}
